d0589d52483a11328b9f69b420fd0040b5196f5b
anil-shelke <142315275+anil-shelke@users.noreply.github.com>
2026-05-27 23:50:28 +0530

    cicd testing

--- backend/src/server.js
+++ backend/src/server.js
@@ -30,7 +30,7 @@ app.use('/api/cart',cartRouter)
 app.use('/api/order',orderRouter)
 
 app.get('/',(req,res)=>{
-    res.send("hello api is working");
+    res.send("hello api is working with cicd");
 })
 
 app.listen(port, ()=>{
